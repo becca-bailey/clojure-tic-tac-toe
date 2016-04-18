34382865e2a9deb2af7490c3666ec085bbcab4a8
Becca Nelson <beccanelson88@gmail.com>
2016-04-18 10:42:59 -0500

    add first test

--- spec/tic_tac_toe/core_spec.clj
+++ spec/tic_tac_toe/core_spec.clj
@@ -3,6 +3,9 @@
             [tic-tac-toe.core :refer :all]
             [tic-tac-toe.ui :refer :all]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(def new-board " 0 | 1 | 2 \n---------\n 3 | 4 | 5 \n---------\n 6 | 7 | 8 ")
+(def initial-board [0 1 2 3 4 5 6 7 8])
+
+(describe "ui"
+  (it "prints an empty board"
+    (should= new-board (display-board initial-board))))
