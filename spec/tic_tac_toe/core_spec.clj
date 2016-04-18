553649bebe0ca939f87e959c8366712cf232215f
Becca Nelson <beccanelson88@gmail.com>
2016-04-18 11:53:58 -0500

    add tests and functions place-marker three-in-a-row and is-winner

--- spec/tic_tac_toe/core_spec.clj
+++ spec/tic_tac_toe/core_spec.clj
@@ -5,7 +5,21 @@
 
 (def new-board " 0 | 1 | 2 \n---------\n 3 | 4 | 5 \n---------\n 6 | 7 | 8 ")
 (def initial-board [0 1 2 3 4 5 6 7 8])
+(def x-wins ["X" 1 2 3 "X" 5 6 7 "X"])
 
-(describe "ui"
-  (it "prints an empty board"
-    (should= new-board (display-board initial-board))))
+(describe "tic tac toe"
+  (context "ui"
+    (it "prints an empty board"
+      (should= new-board (display-board initial-board))))
+
+  (it "places a marker on the board"
+    (should= [0 1 2 3 "X" 5 6 7 8] (place-marker initial-board 4 "X")))
+
+  (context "#three-in-a-row"
+    (it "returns true if the given three spaces have the expected marker"
+      (should= true (three-in-a-row x-wins [0 4 8] "X"))
+      (should= false (three-in-a-row x-wins [0 1 2] "X"))))
+
+  (context "#is-winner"
+    (it "returns true if a given player has won the game"
+      (should= true (is-winner x-wins "X")))))
--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,1 +1,24 @@
 (ns tic-tac-toe.core)
+
+(def winning-combinations [[0 1 2] [3 4 5] [6 7 8]
+                           [0 3 6] [1 4 7] [2 5 8]
+                           [0 4 8] [2 4 6]])
+
+(defn place-marker [board space marker]
+  (assoc board space marker))
+
+(defn three-in-a-row [board set-of-three marker]
+  (loop [possible-win-spots set-of-three]
+    (if (= [] possible-win-spots)
+      true
+      (if (= marker (nth board (first possible-win-spots)))
+        (recur (rest possible-win-spots))
+        false))))
+
+(defn is-winner [board marker]
+  (loop [possible-wins winning-combinations]
+    (if (= [] possible-wins)
+      false
+      (if (three-in-a-row board (first possible-wins) marker)
+        true
+        (recur (rest possible-wins))))))
