c110ae27a7dc5734c8f6d142469c2490b92bc54e
Becca Nelson <beccanelson88@gmail.com>
2016-04-18 15:07:31 -0500

    Extract board logic to separate file

--- spec/tic_tac_toe/core_spec.clj
+++ spec/tic_tac_toe/core_spec.clj
@@ -1,7 +1,8 @@
 (ns tic-tac-toe.core-spec
   (:require [speclj.core :refer :all]
             [tic-tac-toe.core :refer :all]
-            [tic-tac-toe.ui :refer :all]))
+            [tic-tac-toe.ui :refer :all]
+            [tic-tac-toe.board :refer :all]))
 
 (def new-board " 0 | 1 | 2 \n---------\n 3 | 4 | 5 \n---------\n 6 | 7 | 8 ")
 (def initial-board [0 1 2 3 4 5 6 7 8])
@@ -15,24 +16,34 @@
         (should= new-board (display-board initial-board))))
 
     (context "#get-spot"
-      (around [it]
-        (with-out-str (it)))
 
       (it "returns user input as an integer"
         (should= 4
           (with-in-str "4"
-            (get-spot "X"))))))
-    
-  (context "#place-marker"
-    (it "places a marker on the board"
-      (should= [0 1 2 3 "X" 5 6 7 8] (place-marker initial-board 4 "X"))))
+            (get-spot "X")))))
 
-  (context "#three-in-a-row"
-    (it "returns true if the given three spaces have the expected marker"
-      (should= true (three-in-a-row x-wins [0 4 8] "X"))
-      (should= false (three-in-a-row x-wins [0 1 2] "X"))))
+    (context "#display-welcome-message"
 
-  (context "#is-winner"
-    (it "returns true if a given player has won the game"
-      (should= true (is-winner x-wins "X"))
-      (should= true (is-winner o-wins "O")))))
+      (it "prints a welcome message to the console"
+        (should= "Welcome to Tic Tac Toe in Clojure!\n" (with-out-str (display-welcome-message)))))
+
+    (context "#display-winner"
+
+      (it "displays a message with the winner's marker"
+        (should= "X wins!!!\n" (with-out-str (display-winner "X"))))))
+
+  (context "Board"
+
+    (context "#place-marker"
+      (it "places a marker on the board"
+        (should= [0 1 2 3 "X" 5 6 7 8] (place-marker initial-board 4 "X"))))
+
+    (context "#three-in-a-row"
+      (it "returns true if the given three spaces have the expected marker"
+        (should= true (three-in-a-row x-wins [0 4 8] "X"))
+        (should= false (three-in-a-row x-wins [0 1 2] "X"))))
+
+    (context "#is-winner"
+      (it "returns true if a given player has won the game"
+        (should= true (is-winner x-wins "X"))
+        (should= true (is-winner o-wins "O"))))))
---
+++ src/tic_tac_toe/board.clj
@@ -0,0 +1,28 @@
+(ns tic-tac-toe.board)
+
+(def winning-combinations [[0 1 2] [3 4 5] [6 7 8]
+                           [0 3 6] [1 4 7] [2 5 8]
+                           [0 4 8] [2 4 6]])
+
+
+(defn place-marker [board spot marker]
+  (assoc board spot marker))
+
+(defn marker-is-in-spot [board spot marker]
+  (= marker (nth board spot)))
+
+(defn three-in-a-row [board set-of-three marker]
+  (loop [possible-win-spots set-of-three]
+    (if (= [] possible-win-spots)
+      true
+      (if (marker-is-in-spot board (first possible-win-spots) marker)
+        (recur (rest possible-win-spots))
+        false))))
+
+(defn is-winner [board marker]
+  (loop [possible-wins winning-combinations]
+    (if (= [] possible-wins)
+      false
+      (if (three-in-a-row board (first possible-wins) marker)
+        true
+        (recur (rest possible-wins))))))
