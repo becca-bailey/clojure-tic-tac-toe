b6a3791c3cc881ac775d418b4ac0458c00dba3bc
Becca Nelson <beccanelson88@gmail.com>
2016-04-18 16:16:23 -0500

    Add score and switch-player functions

--- src/tic_tac_toe/ai.clj
+++ src/tic_tac_toe/ai.clj
@@ -1,3 +1,12 @@
-(ns tic-tac-toe.ai)
+(ns tic-tac-toe.ai
+  (:require [tic-tac-toe.board :refer :all]
+            [tic-tac-toe.core :refer :all]))
+
+(defn score [board player-marker]
+  (if (is-winner board player-marker)
+    10
+    (if (is-winner board (switch-player player-marker))
+      -10
+      0)))
 
 (defn get-computer-move [board] 7)
