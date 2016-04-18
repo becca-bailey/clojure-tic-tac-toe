011133b43d722cc08f5e1f2c474ca766a6e367b4
Becca Nelson <beccanelson88@gmail.com>
2016-04-18 09:43:04 -0500

    Initial speclj boilerplate

---
+++ spec/tic_tac_toe/core_spec.clj
@@ -0,0 +1,8 @@
+(ns tic-tac-toe.core-spec
+  (:require [speclj.core :refer :all]
+            [tic-tac-toe.core :refer :all]
+            [tic-tac-toe.ui :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
