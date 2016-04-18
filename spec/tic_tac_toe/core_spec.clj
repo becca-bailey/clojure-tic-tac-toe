409fd957865db807b6a73e459c24b840743a3b69
Becca Nelson <beccanelson88@gmail.com>
2016-04-18 14:36:59 -0500

    Add is-winner test for "O" player Add test and function for UI "get spot"

--- spec/tic_tac_toe/core_spec.clj
+++ spec/tic_tac_toe/core_spec.clj
@@ -6,14 +6,26 @@
 (def new-board " 0 | 1 | 2 \n---------\n 3 | 4 | 5 \n---------\n 6 | 7 | 8 ")
 (def initial-board [0 1 2 3 4 5 6 7 8])
 (def x-wins ["X" 1 2 3 "X" 5 6 7 "X"])
+(def o-wins [0 1 2 "O" "O" "O" 7 8 9])
 
 (describe "tic tac toe"
-  (context "ui"
-    (it "prints an empty board"
-      (should= new-board (display-board initial-board))))
+  (context "UI"
+    (context "#display-board"
+      (it "prints an empty board"
+        (should= new-board (display-board initial-board))))
 
-  (it "places a marker on the board"
-    (should= [0 1 2 3 "X" 5 6 7 8] (place-marker initial-board 4 "X")))
+    (context "#get-spot"
+      (around [it]
+        (with-out-str (it)))
+
+      (it "returns user input as an integer"
+        (should= 4
+          (with-in-str "4"
+            (get-spot "X"))))))
+    
+  (context "#place-marker"
+    (it "places a marker on the board"
+      (should= [0 1 2 3 "X" 5 6 7 8] (place-marker initial-board 4 "X"))))
 
   (context "#three-in-a-row"
     (it "returns true if the given three spaces have the expected marker"
@@ -22,4 +34,5 @@
 
   (context "#is-winner"
     (it "returns true if a given player has won the game"
-      (should= true (is-winner x-wins "X")))))
+      (should= true (is-winner x-wins "X"))
+      (should= true (is-winner o-wins "O")))))
--- src/tic_tac_toe/ui.clj
+++ src/tic_tac_toe/ui.clj
@@ -6,3 +6,11 @@
                   " " (nth board 3) " | " (nth board 4) " | " (nth board 5) " \n"
                   "---------\n"
                   " " (nth board 6) " | " (nth board 7) " | " (nth board 8) " ")))
+
+
+(defn get-user-input []
+  (do (flush) (Integer. (read-line))))
+
+(defn get-spot [user-marker]
+   (do (print (str user-marker ": Where would you like to play? "))
+      (get-user-input)))
