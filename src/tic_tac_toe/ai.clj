ac83f378fb6962f06b0d0aeef4d58d7836c29869
Becca Nelson <beccanelson88@gmail.com>
2016-04-18 18:35:17 -0500

    Change core.clj to game.clj, initial test for minimax

--- src/tic_tac_toe/ai.clj
+++ src/tic_tac_toe/ai.clj
@@ -1,6 +1,6 @@
 (ns tic-tac-toe.ai
   (:require [tic-tac-toe.board :refer :all]
-            [tic-tac-toe.core :refer :all]))
+            [tic-tac-toe.game :refer [game-over switch-player progress-game-state]]))
 
 (defn score [board player-marker]
   (if (is-winner board player-marker)
@@ -15,6 +15,8 @@
   (progress-game-state current-game-state
     (place-marker (:board current-game-state) spot (:current-player current-game-state))))
 
-; (defn minimax [spot current-game-state]
-;   (let [scores {}
-;         possible-game (possible-game-state spot current-game-state)]))
+(defn minimax [spot current-game-state]
+  (let [scores {}
+        possible-game (possible-game-state spot current-game-state)]
+    (if (game-over possible-game)
+      (score (:board possible-game) "X"))))
