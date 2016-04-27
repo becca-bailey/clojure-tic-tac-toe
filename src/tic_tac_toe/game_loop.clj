1e8cbaa0156db160cc3c795559d43f774f3229b7
Becca Nelson <beccanelson88@gmail.com>
2016-04-27 14:06:54 -0500

    Add move multimethod and tests
    Add test for computer move

--- src/tic_tac_toe/game_loop.clj
+++ src/tic_tac_toe/game_loop.clj
@@ -5,10 +5,18 @@
             [tic-tac-toe.game :as game]
             [tic-tac-toe.ai :as ai]))
 
-(defn move [game-state]
-  (if (= "X" (game/current-player game-state))
-    (ui/get-spot "X" (board/available-spots (:board game-state)))
-    (ai/best-computer-move game-state)))
+; (defn move [game-state]
+;   (if (= "X" (game/current-player game-state))
+;     (ui/get-spot "X" (board/available-spots (:board game-state)))
+;     (ai/best-computer-move game-state)))
+
+(defmulti move (fn [game-state] (:player-type (game/current-player game-state))))
+
+(defmethod move :default [game-state]
+  (ui/get-spot (:marker (game/current-player game-state)) (board/available-spots (:board game-state))))
+
+(defmethod move :computer [game-state]
+  (ai/best-computer-move game-state))
 
 (def initial-state (game/game-state [0 1 2 3 4 5 6 7 8] "X" 0))
 
@@ -22,8 +30,8 @@
         (ui/confirm-move last-move (game/switch-player (game/current-player game-state))))
       (ui/display-board (:board game-state))
       (cond
-        (game/winner game-state)
-        (ui/display-winner (game/winner game-state))
+        (board/winner game-state)
+        (ui/display-winner (board/winner game-state))
         (board/tie? (:board game-state))
         (ui/display-tie)
         :no-winner-or-tie
