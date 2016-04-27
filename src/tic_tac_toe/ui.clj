70fe95564925f44a20651bf427e25ee14950cc43
Becca Nelson <beccanelson88@gmail.com>
2016-04-27 17:00:25 -0500

    Re-factor UI functions and add more tests
    Extract game setup and add tests
    
    Extract initial-state-with-markers
    
    Temporarily comment game setup specs to avoid infinite looping
    
    add tests for is-a-number-on-board? and is-an-available-spot?
    
    re-work get-user-input and get-spot
    
    Add tests for get-marker-choice multimethod

--- src/tic_tac_toe/ui.clj
+++ src/tic_tac_toe/ui.clj
@@ -1,4 +1,5 @@
-(ns tic-tac-toe.ui)
+(ns tic-tac-toe.ui
+  (:require [tic-tac-toe.player :as player]))
 
 (defn display-board [board]
   (println (str "\n " (first board) " | " (nth board 1) " | " (nth board 2) " \n"
@@ -10,48 +11,32 @@
 (defn display-welcome-message []
   (println "Welcome to Tic Tac Toe in Clojure!"))
 
+(defn is-an-available-spot? [available-spots input]
+  (some (set (map str available-spots)) (vector input)))
+
+(defn print-error [message]
+  (println (str "Sorry, " message ". Try again!")))
+
 (defn is-a-number? [input]
   (some #{"0" "1" "2" "3" "4" "5" "6" "7" "8" "9"} (list input)))
 
-(defn get-user-input [condition error-message]
-  (loop [user-input (read-line)]
+(defn get-user-input
+  ([condition error-message]
+   (loop [user-input (read-line)]
     (if (condition user-input)
       user-input
-      (do (println (str "Sorry, " error-message ". Try again!"))
+      (do (print-error error-message)
         (recur (read-line))))))
-
-(defn get-integer-spot []
-  (Integer. (get-user-input is-a-number? "that's not valid input")))
-
-(defn is-an-available-spot? [available-spots input]
-  (some (set available-spots) (vector input)))
+  ([condition error-message available-spots]
+   (loop [user-input (read-line)]
+    (if (condition available-spots user-input)
+     user-input
+      (do (print-error error-message)
+        (recur (read-line)))))))
 
 (defn get-spot [user-marker available-spots]
-   (println (str user-marker ": Where would you like to play? Available spots: " (clojure.string/join " " available-spots)))
-   (loop [user-input (get-integer-spot)]
-    (if (is-an-available-spot? available-spots user-input)
-      user-input
-      (do (println "Sorry, that spot is already taken. Try again!")
-        (recur (get-integer-spot))))))
-
-(defn is-a-single-character? [input]
-  (= (count input) 1))
-
-(defmulti get-marker-choice :player-type)
-
-(defmethod get-marker-choice :human [player]
-  (println "Choose a character to use as your marker.")
-  (get-user-input is-a-single-character? "your choice must be a single character"))
-
-(defmethod get-marker-choice :computer [player]
-  (println "Choose a character for the computer's marker.")
-  (get-user-input is-a-single-character? "your choice must be a single character"))
-
-(defn get-integer-spot []
-  (Integer. (get-user-input is-a-number? "that's not valid input")))
-
-(defn is-an-available-spot? [available-spots input]
-  (some (set available-spots) (vector input)))
+  (println (str user-marker ": Where would you like to play? Available spots: " (clojure.string/join " " available-spots)))
+  (Integer. (get-user-input is-an-available-spot? "that's not an available spot" available-spots)))
 
 (defn is-a-single-character? [input]
   (= (count input) 1))
